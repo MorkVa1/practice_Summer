c6463906314edfa762a90e4efe53698fbbcb9245
MorkVa1 </>
2022-06-21 22:53:14 +0300

    add complete task

---
+++ base-syntax/task4/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {public static void main(String[] args) {
+        System.out.println("Введите длину последовательности:");
+        Scanner in = new Scanner(System.in);
+        int CountList = in.nextInt();
+        double [] ListOfDigits = new double[CountList];
+        for (int i = 0; i<CountList;i++)
+            ListOfDigits[i] = Math.random();
+        double SumDigits = ListOfDigits[0], MaxDigit = ListOfDigits[0];
+        for (int i = 1; i<CountList;i++)
+        {
+            SumDigits+=ListOfDigits[i];
+            if (MaxDigit<ListOfDigits[i])
+                MaxDigit = ListOfDigits[i];
+        }
+        System.out.printf("Наибольшее значение: %f,\nСреднее значение: %f", MaxDigit, SumDigits/CountList);
+    }
+}
